b0a2184563606474138ff36802e057292e57d4d3
aidenwalker42 <khoul.pixel@gmail.com>
2021-06-28 15:15:03 -0500

    Update test.js

--- test.js
+++ test.js
@@ -3,3 +3,8 @@ const getGreeting = require('./project');
 test('can create a basic greeting', () => {
   expect(getGreeting('Sam')).toBe("Hi Sam");
 });
+
+test('num check', () => {
+  expect(numCheck(2)).toBe(4);
+  
+});
